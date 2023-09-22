cefad0b59c8a3e043b31588f127906a3ff4dde45
José <ffjose2015@gmail.com>
2023-09-21 20:22:15 -0500

    segundo commit

---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+
+
+<template>
+    <Grid>
+        <img :src="url" :alt="nombre">
+        <h2>{{nombre}}</h2>
+        <a :href="url"
+        target="_blank"
+        >Descargar</a>
+    </Grid>
+</template>
+<script>
+import Grid from './Grid.vue';
+
+export default {
+    props: {
+        url: {
+            String
+        },
+        nombre: {
+            String
+        }
+    },
+    components: { Grid }
+}
+</script>
+<style scoped>
+img{
+    width: 8rem;
+    height: 8rem;
+    padding: 0.5rem;
+    background: red;
+}
+a{
+    text-decoration: none;
+    color: snow;
+    background: #301665;
+    padding: 0.3rem;
+    border-radius: 5px;
+    border: 1px solid #f2f2f2;
+}
+</style>
